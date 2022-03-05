649787e6c10b3abaeb2aa57ed42403bd6f5aa628
papi18011998 <papapibrahima98@gmail.com>
2022-03-05 20:36:26 +0000

    model:make repondant  model without function and keys

---
+++ src/models/Repondant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Brief\models;
+
+class Repondant
+{
+    private $id_repondant;
+    private $prenom_repondant;
+    private $nom_repondant;
+    private $telephone_repondant;
+    private $courriel;
+}
