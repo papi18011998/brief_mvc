28c7a57a39ccf3d0103cfc2ae4739ab63006a653
papi18011998 <papapibrahima98@gmail.com>
2022-03-10 09:32:36 +0000

    feature: logout to platform

--- src/views/entreprises/liste.php
+++ src/views/entreprises/liste.php
@@ -13,7 +13,10 @@ if(!isset($_SESSION['user_connected'])){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<div><a href="../utilisateurs/add_organisation" class="btn btn-success">Ajouter une nouvelle organisation</a></div>
+<div class="d-flex justify-content-between border" >
+    <a href="../utilisateurs/add_organisation" class="btn btn-success">Ajouter une nouvelle organisation</a>
+    <a href="../utilisateurs/logout" class="btn btn-danger">Déconnexion</a>
+</div>
 <table class="table table-striped">
   <thead>
     <tr>
